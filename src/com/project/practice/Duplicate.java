fff65ebb20245002d64d701f05ec46dff5bd377b
kalpanaghorpade <kalpanaghorpade77@gmail.com>
2023-01-17 21:36:15 +0530

    added successfully

---
+++ src/com/project/practice/Duplicate.java
@@ -0,0 +1,15 @@
+package com.project.practice;
+
+import java.util.HashSet;
+
+public class Duplicate {
+
+	public static void main(String[] args) {
+		String str="hello all i m learnig java";
+		char[] ch=str.toCharArray();
+		HashSet set=new HashSet();
+		set.add(ch);
+		System.out.println(ch);
+	}
+
+}
